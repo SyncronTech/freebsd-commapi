1a1b8f3721e943a8e9658a6e8db9e18e605ff956
Ari Suutari <ari.suutari@syncrontech.com>
2016-04-11 15:07:00 +0300

    Remove CVS release script.

--- make-release-tar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-	REL=$1
-	RELCVS=`echo $REL | tr . _` 
-	mkdir release
-	cd release
-	cvs export -rRELENG_${RELCVS}_RELEASE -d freebsd-commapi-$REL FreeBSD/commapi
-
-	tar cvzf freebsd-commapi-$REL.tar.gz freebsd-commapi-$REL
